283d2c9afdea49e68385cae88986db378c6a8925
Danilo Chang <ray2501@gmail.com>
2019-11-16 10:34:04 +0800

    Update to 1.7.20

--- build.tcl
+++ build.tcl
@@ -4,7 +4,7 @@
 #package require tls
 
 set arch "x86_64"
-set base "1.7.19"
+set base "1.7.20"
 set fileurl "https://core.tcl-lang.org/tcltls/uv/tcltls-$base.tar.gz"
 
 #http::register https 443 [list ::tls::socket -ssl3 0 -ssl2 0 -tls1 1]
